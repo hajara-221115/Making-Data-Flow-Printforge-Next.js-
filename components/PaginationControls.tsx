02df2d1717b07dc6382708d5291bac34399fdf8b
Hajarabee Shaik <n221115@rguktn.ac.in>
2026-09-11 21:04:26 +0530

    components folder uploaded

---
+++ components/PaginationControls.tsx
@@ -0,0 +1,26 @@
+import PaginationButton from '@/components/PaginationButton'
+
+export default function PaginationControls({totalPages, currentPage}:{
+  totalPages:number,
+  currentPage:number
+}){
+  //[1, 2, 3, 4, 5]
+  const pagesArray = Array.from({length:totalPages}, (_,i) => i + 1)
+  return (
+    <div className="flex justify-center gap-1">  
+      {currentPage !== 1 && (
+        <PaginationButton page={1} label="<<"/>
+      )}
+      {currentPage !== 1 && (
+        <PaginationButton page={currentPage-1}/>
+      )}
+      <PaginationButton page={currentPage} isActive={true}/>
+      {currentPage !== totalPages && (
+        <PaginationButton page={currentPage+1}/>
+      )}
+      {currentPage !== totalPages && (
+        <PaginationButton page={totalPages} label=">>"/>
+      )}
+    </div>
+  )
+}
